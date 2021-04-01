a2ed5dbdb16edb336557d2d02ae80abd1e6d161c
Marlon <14779331+MarlonSchultz@users.noreply.github.com>
2021-04-01 20:06:11 +0200

    got the asic setup with an app running

--- main.tf
+++ main.tf
@@ -12,8 +12,28 @@ provider "aws" {
   region  = "eu-central-1"
 }
 
-resource "aws_cognito_identity_pool" "imageUploader" {
-  name = "imageUploader"
+resource "aws_cognito_user_pool" "userpool" {
+  name              = "imageUploader"
+  mfa_configuration = "ON"
+
+  admin_create_user_config {
+    allow_admin_create_user_only = false
+  }
+
+  alias_attributes = ["email"]
+
+  auto_verified_attributes = ["email"]
+
+  password_policy {
+    minimum_length    = 12
+    require_lowercase = true
+    require_numbers   = true
+    require_symbols   = true
+  }
+
+  software_token_mfa_configuration {
+    enabled = true
+  }
 
   account_recovery_setting {
     recovery_mechanism {
@@ -21,5 +41,14 @@ resource "aws_cognito_identity_pool" "imageUploader" {
       priority = 1
     }
   }
-
 }
+
+resource "aws_cognito_user_pool_client" "testpoolclient" {
+  name         = "testClient"
+  user_pool_id = aws_cognito_user_pool.userpool.id
+
+  logout_urls                  = ["http://localhost"]
+  supported_identity_providers = ["COGNITO"]
+}
+
+
