7187db8b98c57ba1fe9f03c754bf45bfba5f0703
Marlon <14779331+MarlonSchultz@users.noreply.github.com>
2021-04-01 23:40:12 +0200

    added domain to appclient

--- main.tf
+++ main.tf
@@ -20,8 +20,7 @@ resource "aws_cognito_user_pool" "userpool" {
     allow_admin_create_user_only = false
   }
 
-  alias_attributes = ["email"]
-
+  alias_attributes         = ["email"]
   auto_verified_attributes = ["email"]
 
   password_policy {
@@ -43,12 +42,30 @@ resource "aws_cognito_user_pool" "userpool" {
   }
 }
 
+resource "aws_cognito_user_pool_domain" "main" {
+  domain       = "bimmelbommelyolo"
+  user_pool_id = aws_cognito_user_pool.userpool.id
+}
+
 resource "aws_cognito_user_pool_client" "testpoolclient" {
   name         = "testClient"
   user_pool_id = aws_cognito_user_pool.userpool.id
 
-  logout_urls                  = ["http://localhost"]
+  logout_urls   = ["http://localhost"]
+  callback_urls = ["http://localhost"]
+  explicit_auth_flows = ["ALLOW_CUSTOM_AUTH",
+    "ALLOW_REFRESH_TOKEN_AUTH",
+  "ALLOW_USER_SRP_AUTH", ]
+  allowed_oauth_scopes = [
+    "aws.cognito.signin.user.admin",
+  ]
+  allowed_oauth_flows_user_pool_client = true
+  allowed_oauth_flows = [
+    "code",
+  ]
   supported_identity_providers = ["COGNITO"]
 }
+
+
 
 
